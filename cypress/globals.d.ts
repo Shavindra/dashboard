f211370fbcaac3d1ffc2e7cc02b5f48594db877d
Shavindra <shavindra@users.noreply.github.com>
2023-09-12 11:40:30 +0200

    Updated the config

--- cypress/globals.d.ts
+++ cypress/globals.d.ts
@@ -20,59 +20,62 @@ export type CreateUserParams = {
 }
 
 // eslint-disable-next-line no-unused-vars
-declare namespace Cypress {
-  interface Chainable {
+declare global {
 
-    state(state: any): any;
+  namespace Cypress {
+    interface Chainable {
 
-    login(username?: string, password?: string, cacheSession?: boolean): Chainable<Element>;
-    logout(): Chainable;
-    byLabel(label: string): Chainable<Element>;
+      state(state: any): any;
 
-    createUser(params: CreateUserParams): Chainable;
-    setGlobalRoleBinding(userId: string, role: string): Chainable;
-    setClusterRoleBinding(clusterId: string, userPrincipalId: string, role: string): Chainable;
-    setProjectRoleBinding(clusterId: string, userPrincipalId: string, projectName: string, role: string): Chainable;
-    getProjectByName(clusterId: string, projectName: string): Chainable;
+      login(username?: string, password?: string, cacheSession?: boolean): Chainable<Element>;
+      logout(): Chainable;
+      byLabel(label: string): Chainable<Element>;
 
-    getRancherResource(prefix: 'v3' | 'v1', resourceType: string, resourceId: string, expectedStatusCode: string): Chainable;
-    setRancherResource(prefix: 'v3' | 'v1', resourceType: string, resourceId: string, body: string): Chainable;
+      createUser(params: CreateUserParams): Chainable;
+      setGlobalRoleBinding(userId: string, role: string): Chainable;
+      setClusterRoleBinding(clusterId: string, userPrincipalId: string, role: string): Chainable;
+      setProjectRoleBinding(clusterId: string, userPrincipalId: string, projectName: string, role: string): Chainable;
+      getProjectByName(clusterId: string, projectName: string): Chainable;
 
-    /**
-     *  Wrapper for cy.get() to simply define the data-testid value that allows you to pass a matcher to find the element.
-     * @param id Value used for the data-testid attribute of the element.
-     * @param matcher Matching character used for attribute value:
-     * - `$`: Suffixed with this value
-     * - `^`: Prefixed with this value
-     * - `~`: Contains this value as whitespace separated words
-     * - `*`: Contains this value
-     */
-    getId(id: string, matcher?: Matcher): Chainable<Element>;
+      getRancherResource(prefix: 'v3' | 'v1', resourceType: string, resourceId: string, expectedStatusCode: string): Chainable;
+      setRancherResource(prefix: 'v3' | 'v1', resourceType: string, resourceId: string, body: string): Chainable;
 
-    /**
-     *  Wrapper for cy.find() to simply define the data-testid value that allows you to pass a matcher to find the element.
-     * @param id Value used for the data-testid attribute of the element.
-     * @param matcher Matching character used for attribute value:
-     * - `$`: Suffixed with this value
-     * - `^`: Prefixed with this value
-     * - `~`: Contains this value as whitespace separated words
-     * - `*`: Contains this value
-     */
-    findId(id: string, matcher?: Matcher): Chainable<Element>;
+      /**
+       *  Wrapper for cy.get() to simply define the data-testid value that allows you to pass a matcher to find the element.
+       * @param id Value used for the data-testid attribute of the element.
+       * @param matcher Matching character used for attribute value:
+       * - `$`: Suffixed with this value
+       * - `^`: Prefixed with this value
+       * - `~`: Contains this value as whitespace separated words
+       * - `*`: Contains this value
+       */
+      getId(id: string, matcher?: Matcher): Chainable<Element>;
 
-    /**
-     * Override user preferences to default values, allowing to pass custom preferences for a deterministic scenario
-     * Leave empty for reset to default values
-     */
-    // eslint-disable-next-line no-undef
-    userPreferences(preferences?: Partial<UserPreferences>): Chainable<null>;
+      /**
+       *  Wrapper for cy.find() to simply define the data-testid value that allows you to pass a matcher to find the element.
+       * @param id Value used for the data-testid attribute of the element.
+       * @param matcher Matching character used for attribute value:
+       * - `$`: Suffixed with this value
+       * - `^`: Prefixed with this value
+       * - `~`: Contains this value as whitespace separated words
+       * - `*`: Contains this value
+       */
+      findId(id: string, matcher?: Matcher): Chainable<Element>;
 
-    requestBase64Image(url: string): Chainable;
+      /**
+       * Override user preferences to default values, allowing to pass custom preferences for a deterministic scenario
+       * Leave empty for reset to default values
+       */
+      // eslint-disable-next-line no-undef
+      userPreferences(preferences?: Partial<UserPreferences>): Chainable<null>;
 
-    keyboardControls(triggerKeys: Partial<any>, count: number): Chainable<Element>;
+      requestBase64Image(url: string): Chainable;
 
-    interceptAllRequests(verbs: Verbs, urls: string[], timeout: number): Chainable<string>;
+      keyboardControls(triggerKeys: Partial<any>, count: number): Chainable<Element>;
 
-    iFrame(): Chainable<Element>;
+      interceptAllRequests(verbs: Verbs, urls: string[], timeout: number): Chainable<string>;
+
+      iFrame(): Chainable<Element>;
+    }
   }
 }
